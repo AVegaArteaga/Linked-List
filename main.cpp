698e990ee4bf5af5dc0046b0f5e5e65c9e247fee
Abel Vega Arteaga <abelvega102@gmail.com>
2022-02-25 03:27:58 -0600

    Added bubble sort

--- main.cpp
+++ main.cpp
@@ -1,155 +1,190 @@
+
+// C++ program to delete a node in
+// singly linked list recursively
+ 
 #include <bits/stdc++.h>
-
 using namespace std;
-
-// struct Car{
-//     string model;
-//     string maker;
-//     string 
-
-// };
-
-class Node {
-    public:
-        int data;
-  
-  Node *next;
+ 
+struct node {
+    int info;
+    node* link = NULL;
+    node() {}
+    node(int a) : info(a) {}
 };
-
-/* Given a node prev_node, insert a new node after the given
-prev_node */
-void insertAfter(Node* prev_node, int new_data)
-{
-    /*1. check if the given prev_node is NULL */
-    if (prev_node == NULL)
-    {
-        cout<<"The given previous node cannot be NULL";
-        return;
-    }
  
-    /* 2. allocate new node */
-    Node* new_node = new Node();
- 
-    /* 3. put in the data */
-    new_node->data = new_data;
- 
-    /* 4. Make next of new node as next of prev_node */
-    new_node->next = prev_node->next;
- 
-    /* 5. move the next of prev_node as new_node */
-    prev_node->next = new_node;
-}
+ int list_size = 0;
 
-void append(Node** head_ref, int new_data){
-    /* 1. allocate node */
-    Node* new_node = new Node();
+ void print(node * n) {
 
-    Node *last = *head_ref; /* used in step 5*/
- 
-    // |Data: n | Next: ?|
-    /* 2. setting the data for the new node */
-    new_node->data = new_data;
-
-    // |Data: n | Next: NULL|
-    /* 3. Since this is going to be the tail, ei the end, the next node would be NULL*/
-    new_node->next = NULL;
- 
-    /* 4. If the Linked List is empty,
-    then make the new node as head */
-    if (*head_ref == NULL){
-        *head_ref = new_node;
-        return;
-    }
- 
-    /* 5. Else traverse till the last node. Traverse will be taxxing on cpu*/
-    while (last->next != NULL)
-        last = last->next;
- 
-    /* 6. Change the next of last node */
-    last->next = new_node;
-    return;
-
-
-}
-
-void print_list(Node * n) {
-
-    //cout << "          v HEAD v" << endl;
+    int index = 0;
     while (n != NULL) {
-        //cout << "|Data: " << n->data << "|Node: " << n << "| Next: ";
-        cout << "|Data: " << n->data << "|";
+        cout << "|Data: " << n->info << "|Node: " << n << "| Next: ";
+        //cout << setw(3) << index++ << "=|Data:" << setw(3) <<n->info << "|";
 
 
-        if(n->next == NULL)
+        if(n->link == NULL)
             cout << "NULL";
         else
-            //cout << n->next;
+            cout << n->link;
 
         cout << "\n";
 
-        n = n->next;
+        n = n->link;
     }
-    //cout << "                                   ^ TAIL ^" << endl;
     cout << endl;
 }
 
-/* Given a reference (pointer to pointer)
-to the head of a list and an int,
-inserts a new node on the front of the list. */
-void push(Node** head_ref, int new_data)
+/* Function to swap data of two nodes a and b*/
+node* swap2(node* a, node* b){
+   
+    node* tmp = b->link;
+    b->link = a;
+    a->link = tmp;
+    return b;
+}
+
+/* Function to bubble sort the given linked list */
+int bubbleSort(node** head){
+
+ 
+    bool isSwapped = false;
+
+    for(int i = 0; i <= list_size; i++){ //pass and there is a hard limit of list_size
+        node** h = head; //resets the head
+        isSwapped = false;
+        
+        for(int j = 0; j < list_size - i - 1; j++){
+            
+            node *p1 = *h;
+            node *p2 = p1->link;
+
+           
+            if(p1->info > p2->info){
+               
+                *h = swap2(p1,p2);
+                isSwapped = true;
+            }
+        
+            
+            h = &(*h)->link;
+        }
+
+
+        if( !isSwapped )  //we went through the linked list without swapping, no lets stop
+            break;
+
+    }
+
+}
+  
+
+/*
+Deletes the node containing 'info' part as val and
+alter the head of the linked list (recursive method)
+*/
+void deleteNode(node*& head, int val)
 {
-    /* 1. allocate node */
-    Node* new_node = new Node();
- 
-    /* 2. put in the data */
-    new_node->data = new_data;
- 
-    /* 3. Make next of new node as head */
-    new_node->next = (*head_ref);
- 
-    /* 4. move the head to point to the new node */
-    // cout << "*head_ref: " << *head_ref << endl;
-    // cout << "*new_node: " << new_node << endl;
-    *head_ref = new_node;
+    //checks if the list is empty or we are already went through the list
+    if (head == NULL) {
+        cout << "Element not present in the list\n";
+        return;
+    }
+    // If Found, we delete 
+    if (head->info == val) {
+        node* t = head;
+        head = head->link; // If it's start of the node head
+                           // node points to second node
+        delete (t); // Else changes previous node's link to
+                    // current node's link
+        return;
+    }
+    //else keep searching
+    deleteNode(head->link, val);
 }
  
-// This code is contributed by rathbhupendra
+// Utility function to add a
+// node in the linked list
+// Here we are passing head by
+// reference thus no need to
+// return it to the main function
+void push(node*& head, int data)
+{
+    node* newNode = new node(data);
+    newNode->link = head;
+    head = newNode;
+}
+ 
+
+// Given a reference (pointer to pointer) to
+// the head of a list and a position, deletes
+// the node at the given position
+void deletePosNode(node** head_ref, int position)
+{
+ 
+    // If linked list is empty
+    if (*head_ref == NULL)
+        return;
+ 
+    // Store head node
+    node* temp = *head_ref;
+ 
+    // If head needs to be removed
+    if (position == 0) {
+ 
+        // Change head
+        *head_ref = temp->link;
+ 
+        // Free old head
+        free(temp);
+        return;
+    }
+ 
+    // Find previous node of the node to be deleted
+    for (int i = 0; temp != NULL && i < position - 1; i++)
+        temp = temp->link;
+ 
+    // If position is more than number of nodes
+    if (temp == NULL || temp->link == NULL)
+        return;
+ 
+    // Node temp->next is the node to be deleted
+    // Store pointer to the next of node to be deleted
+    node* next = temp->link->link;
+ 
+    // Unlink the node from linked list
+    free(temp->link); // Free memory
+ 
+    // Unlink the deleted node from list
+    temp->link = next;
+}
+
+void enterFile(char *file, node*& head_ref){
+
+    int num;
+	ifstream infile;  
+
+	infile.open(file);					
+	if (!infile) {cout << file << " did not open!" << endl; exit(-1);}
+	
+    while (infile >> num){
+        list_size++;
+        push(head_ref, num);
+        if(list_size == 10)
+            break;
+    }
+	infile.close();                   // after we are done with the input text, we close the file. The program will close it after its done, but its good practice
+
+}
 
 int main(int argc, char *argv[]){
 
-    Node * head = NULL;
-    int num;
+    node * head = NULL;
 
-	ifstream infile;                                
-	infile.open(argv[1]);					
-	if (!infile) {cout << "input.txt did not open"; exit(-1);} 												 // exits out
-	infile >> num;                                  // takes the first word of the input and puts it in 'word' temperally
-	while (infile) {                                 // loop until hit the end of line, i.e. hits the '\o' it will get out of the loop
-        push(&head, num);
-		infile >> num;              
-	}
-	infile.close();                   // after we are done with the input text, we close the file. The program will close it after its done, but its good practice.
-					
-    insertAfter(head->next, 8);
-    //push(&head, 7);
+    enterFile(argv[1], head);
+    print(head);
+    bubbleSort(&head);
+    print(head);
 
 
-    // Node * second = NULL;
-    // Node * third = NULL;
-
-    // head = new Node();
-    // second = new Node();
-    // third = new Node();
-
-    // head->data = 1;
-    // head->next = second;
-
-    // second->data = 2;
-    // second->next = third;
-
-    // third->data = 3;
-    // third->next = NULL;
-
-    print_list(head);
-
+    return 0;
 }
